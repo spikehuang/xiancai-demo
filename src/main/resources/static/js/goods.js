1b9bb746f116008d338d6b9986fe77bc3ea22e72
spikehuang <hh_7@foxmail.com>
2018-05-25 01:15:08 +0800

    add login module

--- src/main/resources/static/js/goods.js
+++ src/main/resources/static/js/goods.js
@@ -1,16 +1,31 @@
 $(function() {
 
     $(document).ready(function() {
+        // Cookies.set('username', 'tom', {expires: 7, path: '/xiancai'});
+        // Cookies.set('userId', 7001, {expires: 7, path: '/xiancai'});
         init();
     });
 
     function init() {
-        var loginModal = $('#loginModal');
-        loginModal.modal({
-            show: true,
-            backdrop: 'static',
-            keyboard: false
-        });
+        var currentUser = Cookies.get('username');
+        if (!currentUser) {
+            var loginModal = $('#loginModal');
+            loginModal.modal({
+                show: true,
+                backdrop: 'static',
+                keyboard: false
+            });
+        }
+
+        var loginNavBar = $('.navbar-right');
+        loginNavBar.empty();
+        loginNavBar.html('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' + currentUser + '<span class="caret"></span></a><ul class="dropdown-menu"><li><a href="/xiancai/home">查看个人信息</a></li><li><a href="#">已购商品</a></li><li><a href="#">收藏夹</a></li><li role="separator" class="divider"></li><li><a href="#" id="logOut">退出登录</a></li></ul></li>');
     }
+    
+    $('.navbar-right').on('click', '#logOut', function () {
+        Cookies.remove("username", { path: '/xiancai' });
+        Cookies.remove("userId", { path: '/xiancai' });
+        location.href = '/xiancai';
+    });
 
 });
